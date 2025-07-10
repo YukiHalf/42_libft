03bc3c7bed563b1d2f5b1805a49f185f9bdae836
YukiHalf <simutdarius7@gmail.com>
2025-07-10 14:42:10 +0200

    added more things

--- ft_strjoin.c
+++ ft_strjoin.c
@@ -6,8 +6,31 @@
 /*   By: sdarius- <sdarius-@student.42heilbronn.    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/07/10 14:29:35 by sdarius-          #+#    #+#             */
-/*   Updated: 2025/07/10 14:29:44 by sdarius-         ###   ########.fr       */
+/*   Updated: 2025/07/10 14:41:38 by sdarius-         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
+
+char	*ft_strjoin(const char *s1, const char *s2)
+{
+	char	*str;
+	size_t	i;
+
+	str = (char *)malloc((ft_strlen(s1) + ft_strlen(s2) + 1) * sizeof(char));
+	i = 0;
+	while (*s1)
+	{
+		str[i] = *s1;
+		s1++;
+		i++;
+	}
+	while (*s2)
+	{
+		str[i] = *s2;
+		s2++;
+		i++;
+	}
+	str[i] = 0;
+	return (str);
+}
